b8d448d7070c18aaaa794a07008a29421ea6feaa
mateoleont <mateolt505@gmail.com>
2023-09-06 00:50:32 -0500

    images

--- client/src/views/Landing/landing.jsx
+++ client/src/views/Landing/landing.jsx
@@ -1,18 +1,25 @@
 // Este componente es el Landing Page
 //----------------------------------------------
 import './landing.css';
-import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 //----------------------------------------------
 const Landing = () =>
 {
+    const history = useHistory();
+
+    const handleButtonClick = () => 
+    {
+        history.push('/home'); // Redirige al enlace '/home' al hacer clic en el botón
+    };
+
     return(
     <>
     <div className = "landing-images">
-    <img src = "https://phantom-marca.unidadeditorial.es/10c286b7acf9a5a3bc72be2862268068/resize/1200/f/jpg/assets/multimedia/imagenes/2021/02/28/16145196328904.jpg" alt = "Pokemon APP"/>
+    <img src = "../../images/landing-image.jpg" alt = "Pokemon APP"/>
     </div>
     <br />
-    <button className='landingButton'>
-        <NavLink to ='/home'> Vamos! </NavLink> 
+    <button className='landingButton' onClick = {handleButtonClick}>
+        Vamos!
     </button>
     </>
     );
