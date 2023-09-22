4fc846858aab4dfb74d8b27a85dc3a700d2a1ad9
mateoleont <mateolt505@gmail.com>
2023-09-22 16:35:30 -0500

    casi listo

--- client/src/Components/pokemonCard/card.jsx
+++ client/src/Components/pokemonCard/card.jsx
@@ -8,11 +8,19 @@ import './card.css';
 const Card = (props) =>
 {
     return (
-        <div className = 'cardContainer'>
-            <p className = 'cardName'> {props.name}</p>
-            <img src = {props.sprites} alt = {props.name} className = "cardImage" />
-            <div className = "cardStats">
-
+        <div className = 'container'>
+            <div className = 'cardContainer'>
+                <div className = 'cardHeader'>
+                    <p className = 'cardName'> {props.name}</p>
+                </div>
+                <div className = 'cardImageContainer'>
+                    <img src = {props.sprites} alt = {props.name} className = "cardImage" />
+                </div>
+                <div className = 'cardDescription'>
+                    <div className = "cardStats">
+                        <p>Types: {props.types}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
